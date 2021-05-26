3ead9289a6e34cfd30c8614fb6fdd06262133170
Kevin Clement <kevin.clement@gmail.com>
2021-05-26 01:31:02 -0700

    initial checkin from prototype

---
+++ wiz.py
@@ -0,0 +1,32 @@
+# Support for alternative wifi bulb - wiz
+# to make this work I installed
+#   pip3 install asyncio
+#   pip3 install pywizlight
+
+import sys
+import asyncio
+from pywizlight import wizlight, PilotBuilder, discovery
+
+# allow to pass 'red' or 'white' as color in arguments, otherwise default to white
+color = sys.argv[1] if len(sys.argv) > 1 else "white" 
+if color != "white" and color != "red":
+    color = "white"
+
+pb = PilotBuilder(warm_white = 255)
+if color == "red":
+    pb = PilotBuilder(rgb = (255, 0, 0))
+
+pb.brightness = 255
+pb.speed = 100
+
+###########################################
+# NOTE: bulb is hard coded to this IP so  #
+# that I don't have to discover it        #
+###########################################
+
+async def main():
+    light = wizlight("192.168.1.26")
+    await light.turn_on(pb)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
