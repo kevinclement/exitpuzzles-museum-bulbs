7cdc77e14ddb64d26abc954e8d6886726cf10e15
Kevin Clement <kevin.clement@gmail.com>
2021-06-02 11:26:52 -0700

    add comment for extra bulb

--- bulbs.color.js
+++ bulbs.color.js
@@ -32,6 +32,13 @@ exports.colors = {
 
         return `56${r}${g}${b}${w}aa3b070001`
     }
+
+    // NOTE: below is string used for 'MINGER' bulb after a night of debugging
+    //  This is in case we want to use that one as a backup.  I have one still around
+    //  command I used when manually running
+    //    /usr/bin/gatttool -b A4:C1:38:A0:4C:22 --char-write-req -a 0x0015 -n '3305020000ff00000000000000000000000000cb'
+    //  format is:
+    //    '330502${r}${g}${b}00000000000000000000000000cb'
 }
 
 function getRandomInt(max) {
