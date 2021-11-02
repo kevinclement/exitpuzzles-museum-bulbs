a1ee304ef6decd187e23e4063540ed6b28c82d68
Kevin Clement <kevin.clement@gmail.com>
2021-11-01 19:18:33 -0700

    add color for red and blue

--- wiz.py
+++ wiz.py
@@ -5,7 +5,7 @@
 
 # Usage
 #   wiz.py <IP> <COLOR>
-#     COLOR - only supports "red" or "r"
+#     COLOR - only supports "red" or "r", "blue" or "b", "green" or "g"
 #             defaults to "white"
 
 import sys
@@ -14,22 +14,26 @@ from pywizlight import wizlight, PilotBuilder, discovery
 
 addr = sys.argv[1]
 
-# allow to pass 'red' or 'white' as color in arguments, otherwise default to white
+# allow to pass 'red', 'blue', 'green', 'white' as color in arguments, otherwise default to white
 color = sys.argv[2] if len(sys.argv) > 2 else "white" 
-if color != "white" and color != "red" and color != "r":
+if (color != "white" and 
+    color != "red" and color != "r" and 
+    color != "blue" and color != "b" and
+    color != "green" and color != "g"):
     color = "white"
 
-pb = PilotBuilder(warm_white = 230, brightness = 140, speed = 100)
-if color == "red" or color == "r":
-    pb = PilotBuilder(rgb = (255, 0, 0), brightness = 255, speed = 100)
-
-###########################################
-# NOTE: bulb is hard coded to this IP so  #
-# that I don't have to discover it        #
-###########################################
+pb = PilotBuilder(warm_white = 230, brightness = 110, speed = 100)
+if (color != "white"):
+    if color == "red" or color == "r":
+        pb = PilotBuilder(rgb = (255, 0, 0), brightness = 255, speed = 100)
+    elif color == "green" or color == "g":
+        pb = PilotBuilder(rgb = (0, 255, 0), brightness = 255, speed = 100)    
+    elif color == "blue" or color == "b":
+        pb = PilotBuilder(rgb = (0, 0, 255), brightness = 255, speed = 100)
+    else:
+        raise ValueError("color wasn't white but couldn't parse to true color")
 
 async def main():
-    # 6.3
     light = wizlight(addr)
     await light.turn_on(pb)
 
