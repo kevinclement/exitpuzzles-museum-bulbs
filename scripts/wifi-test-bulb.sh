4a16163c5f6749333084ae1f5c0a66b73b075209
Kevin Clement <kevin.clement@gmail.com>
2021-11-25 10:35:00 -0800

    add test script for wifi when adding bulb

---
+++ scripts/wifi-test-bulb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 192.168.1.200+
+# red,blue,green,white
+# /usr/bin/python3 /home/pi/code/bulbs/wiz.py ${addr} ${colorFriendly}
+/usr/bin/python3 /home/pi/code/bulbs/wiz.py 192.168.1.203 green
+
